2eeeff21c2231acd5d6609f5ef9b05771b003a1c
Nick Kibysh <nick.kibysh@gmail.com>
2023-03-30 16:52:22 +0200

    Initial commit

---
+++ Sources/iOS-Bluetooth-Numbers-Database/iOS_Bluetooth_Numbers_Database.swift
@@ -0,0 +1,6 @@
+public struct iOS_Bluetooth_Numbers_Database {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/iOS-Bluetooth-Numbers-DatabaseTests/iOS_Bluetooth_Numbers_DatabaseTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iOS_Bluetooth_Numbers_Database
+
+final class iOS_Bluetooth_Numbers_DatabaseTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iOS_Bluetooth_Numbers_Database().text, "Hello, World!")
+    }
+}
